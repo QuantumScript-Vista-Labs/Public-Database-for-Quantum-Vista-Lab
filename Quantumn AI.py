45084a6bbca79f8173214e4bde974ccb577d26dc
vistadev265 <165598045+vistadev265@users.noreply.github.com>
2024-04-07 18:55:39 -0700

    Quantum AI

---
+++ Quantumn AI.py
@@ -0,0 +1,107 @@
+print("Quantumn AI©")
+print("Chat is not saved here")
+print("this also is s developer or advanced users can use to read the messages")
+print("ask me anything.")
+re = input()
+if re == "vistadev":
+    print("He started making all of these games including this AI.")
+    input()
+elif re == "vista beat":
+    print("It is a game like Beat Saber but in 2D and no VR required.")
+    input()
+elif re == "when did vista make his first game":
+    print("he make his first game ever in 2021")
+    input()
+elif re == "reactos":
+    print("it is an open source oprating system based on the windows nt kernal. they are an independant from Microsoft and do not use it on your main machiene or backup everythong and continue with setup.")
+    input()
+elif re == "quantum script vista lab":
+    print("it is a reserch non-profit buissness focasing on python, cpu development, and ai.")
+    input()
+elif re == "what is this called":
+    print("it is called Quantumn AI that is made by one person vista.")
+    input()
+elif re == "Quantumn AI":
+    print("It is an AI developed by Quantum Script Vista Lab")
+    input()
+elif re == "what is the bestest compaq":
+    print("it depends on what compaq it is but my favorite compaq is the f500")
+    input()
+# this is ment for devs only that know how to code in python
+elif re == "ex":
+    ex = input("put the code in here it'll atomaticaly execute the code:\n")
+    exec(ex)
+elif re == "what is python":
+    print("it is a developing platform and is considered the easiest programing language and i was developed on it")
+elif re == "dani":
+    print("He is the milk god and made the oculas milk to make games on 3 times.")
+    input()
+elif re == "where is dani":
+    print("he is developing karlson it might relese at somepoint in time")
+    input()
+elif re == "AI":
+    print("it is Artifital Intelegence which means it is like a brain")
+    input()
+elif re == "what is intel":
+    print("it is somthing that the other side has that was top secret")
+    input()
+elif re == "TF2":
+    print("It is a game called Team Fortress 2 where you try to take the (brefcase) intelegence")
+    input()
+elif re == "how can i use the api":
+    print("you can import it in python")
+    input()
+elif re == "features coming soon":
+    print("i am only text based ai but here are some features coming(Genorating images, being able to code only in python, open browser)")
+    input()
+elif re == "what is it being an AI":
+    print("It is good soo far but i might need more information")
+    input()
+elif re == "where can I get more software":
+    print("you can get them at vistadev.itch.io")
+    input()
+elif re == "what is bing":
+    print("bing is a prouduct from microsoft that has copilot amd though is not that popular")
+    input()
+elif re == "is this AI use machiene learning":
+    print("No it is pretyped questions that the developer can answer instead of machiene learning")
+    input()
+elif re == "Why does this even exist":
+    print("Its main goal is to tell you whats possible via typeing")
+    input()
+elif re == "what does BIOS mean":
+    print("it mean's basic input-output")
+    input()
+elif re == "who is Green Day":
+    print("Green Day is a band that are nutorious for their songs like wake me up when setember ends and american idiot.")
+    input()
+elif re == "what is pride":
+    print("pride is where people celebrate their diffences in sexuallity")
+    input()
+elif re == "what computer made this AI":
+    print("a hp stream 11 2015")
+    input()
+elif re == "count 1 through 10":
+    print ("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+    input()
+elif re == "why use python to develope this app":
+    print("It is easier to make stuff using print and elif like")
+    input()
+elif re == "why is it slow on startup but fast when it conts to 10":
+    print("It has been pre typed everything")
+    input()
+elif re == "how long is the code for this":
+    print("100 lines possibly")
+elif re == "write 100 vista":
+    print("ok")
+    print("vista "*100)
+elif re == "write 100 of what i say":
+    print("type here")
+    word = input()
+    print("how much")
+    how_much = input()
+    print(word*how_much)
+
+else:
+    print("i don't know what you mean by that")
+    input()
